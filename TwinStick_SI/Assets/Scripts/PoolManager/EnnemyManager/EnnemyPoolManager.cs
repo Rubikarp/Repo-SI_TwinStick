77f32bfc84ccfbb857b645f6f3fcb9f04076ba38
MichaelAbid <91900412+MichaelAbid@users.noreply.github.com>
2022-04-27 10:43:46 +0200

    Navmesh unBugged

--- TwinStick_SI/Assets/Scripts/PoolManager/EnnemyManager/EnnemyPoolManager.cs
+++ TwinStick_SI/Assets/Scripts/PoolManager/EnnemyManager/EnnemyPoolManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using NaughtyAttributes;
 
 [Serializable]
 public struct EnnemyPercent
@@ -29,17 +29,19 @@ public class EnnemyPoolManager : Singleton<EnnemyPoolManager>
     [SerializeField]
     public List<EnnemyPercent> listOfEnnemyType;
     public int numberMultipier = 10;
+    
     #endregion
 
     #region Internal Variables
     [HideInInspector]
     public List<GameObject> listOfCreatedEnnemy;
-
+    
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        listOfCreatedEnnemy = new List<GameObject>();
         // Generate a bunch of Ennemy based of listOfEnnemyType, We will multiply numberMultiplier by theur respective spawnForce to have a large amount of created ennemy
         foreach (EnnemyPercent typeOfEnnemy in listOfEnnemyType){
             CreateEnnemy(typeOfEnnemy.ennemyPrefab, typeOfEnnemy.spawnForce*numberMultipier);
@@ -47,12 +49,23 @@ public class EnnemyPoolManager : Singleton<EnnemyPoolManager>
         
 
     }
+    [Button]
+    void GenerateEnnemyAtStart()
+    {
+
+        
+
+        foreach (EnnemyPercent typeOfEnnemy in listOfEnnemyType)
+        {
+            CreateEnnemy(typeOfEnnemy.ennemyPrefab, typeOfEnnemy.spawnForce * numberMultipier);
+        }
+    }
 
     bool CreateEnnemy(UnityEngine.Object type,int nb)
     {
         for (int i = 0; i < nb; i++)
         {
-            GameObject ennemyCreated = (GameObject)Instantiate(type, new Vector3(-500, -500, -500), Quaternion.identity);
+            GameObject ennemyCreated = (GameObject)Instantiate(type, new Vector3(0,0,0), Quaternion.identity);
             ennemyCreated.name = "Ennemy #" + (listOfCreatedEnnemy.Count);
             ennemyCreated.transform.SetParent(gameObject.transform);
             ennemyCreated.GetComponent<AEnnemy>().currentState = AI_STATE.AI_STATE_IN_POOL;
@@ -123,7 +136,7 @@ public class EnnemyPoolManager : Singleton<EnnemyPoolManager>
             float safezonex = UnityEngine.Random.Range(-ennemyToSpawn.Count, ennemyToSpawn.Count);
             float safezonez = UnityEngine.Random.Range(-ennemyToSpawn.Count, ennemyToSpawn.Count);
             ennemy.transform.position = parent.position + new Vector3(safezonex, 1.1f, safezonez);
-
+            
             ennemyComp.FindNewTarget();
             yield return new WaitForSeconds(0.2f);
         }
